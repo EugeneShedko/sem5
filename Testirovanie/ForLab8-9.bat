cb27e8d5038938737fbe28fdaf39185ee3299696
Eugene Shedko <evgenij-2002@bk.ru>
2022-02-11 22:41:53 +0300

    Zaschita

---
+++ Testirovanie/ForLab8-9.bat
@@ -0,0 +1,3 @@
+
+cd /d D:/Programs/Jenkins
+java -jar agent.jar -jnlpUrl http://34.125.50.211:8080/computer/DESKTOP-H66HRKQ/jenkins-agent.jnlp -secret b619562f3769c2647224a3374a427e1f1a5723f296d0f3702f0288ffcc1ed0ae -workDir "D:\Programs\Jenkins"
